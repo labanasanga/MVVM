48710d5b7d68b8379ad35e14594f0a1a2b1835c8
laban <labanasanga@icloud.com>
2024-04-10 03:22:39 +0300

    Initial commit

---
+++ app/src/main/java/com/labanasanga/mvvm/navigation/Routes.kt
@@ -0,0 +1,5 @@
+package com.labanasanga.mvvm.navigation
+
+const val ROUTE_HOME="home"
+const val ROUTE_ABOUT="about"
+
---
+++ app/src/main/java/com/labanasanga/mvvm/ui/theme/screen/about/AboutScreen.kt
@@ -0,0 +1,37 @@
+package com.labanasanga.mvvm.ui.theme.screen.about
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun abtscreen() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Gray)
+    ) {
+        Text(text = "This is About Screen",
+            fontSize = 40.sp,
+            fontFamily = FontFamily.SansSerif,
+            color = Color.Black)
+    }
+}
+
+@Preview
+@Composable
+private fun Abtprev() {
+    abtscreen()
+}
